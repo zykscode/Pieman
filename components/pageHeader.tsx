710c2dbb1dbc83d928989a5a3d5bf9eb0d91dd3f
zykcode <zyskson@gmail.com>
2024-05-18 17:43:59 +0100

    seju

--- components/pageHeader.tsx
+++ components/pageHeader.tsx
@@ -5,9 +5,7 @@ import { marketingConfig } from '../config/marketing'
 import Breadcrumbs from './breadcrumbs'
 import Navs from './navs'
 
-type Props = {}
-
-const PageHeader = (props: Props) => {
+const PageHeader = () => {
    return (
     <header className="z-50 rounded-3xl w-full  fixed bottom-0 ">
     <div className="nav-header  flex justify-between gap-4 ">
