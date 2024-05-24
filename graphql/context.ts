f97a94fd393bd5aa2babb4326958afb1b2175d4a
zykcode <zyskson@gmail.com>
2024-05-24 21:45:26 +0100

    tired

--- graphql/context.ts
+++ graphql/context.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from '@prisma/client';
 
 interface Context {
   prisma: PrismaClient;
-  // You can add other context properties as needed, e.g., user, token, etc.
 }
 
 export default Context;
