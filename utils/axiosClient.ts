c084c067b847bb066fccf19ba2a9abcc689a44d1
zykcode <zyskson@gmail.com>
2024-06-22 16:29:52 +0100

    ilesse

--- utils/axiosClient.ts
+++ utils/axiosClient.ts
@@ -1,6 +1,17 @@
-// src/utils.ts
-import { AxiosClientOptions } from "#/types";
-import axios, { AxiosInstance } from "axios";
+import axios from 'axios';
+
+const axiosClient = axios.create({
+  baseURL: process.env.PI_API_KEY || '/api',
+  timeout: 20000,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }
+});
+
+export default axiosClient;
+
 
 export const getAxiosClient = (apiKey: string, options: AxiosClientOptions | null): AxiosInstance => {
   return axios.create({
