7f1d4d99aaef34cb7847b016c992c449c7cc45a3
zykcode <zyskson@gmail.com>
2024-06-23 01:14:40 +0100

    ilesse

--- app/api/verify-auth/route.ts
+++ app/api/verify-auth/route.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
