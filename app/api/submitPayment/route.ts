fab1169c22767953badfeef3cb7f9b0b04c89310
zykcode <zyskson@gmail.com>
2024-06-25 15:06:52 +0100

    school

--- app/api/submitPayment/route.ts
+++ app/api/submitPayment/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
 
     // Update transaction with txid
     await prisma.transaction.updateMany({
-      where: { id: paymentId },
+      where: { paymentId: paymentId },
       data: { id: txid },
     });
 
