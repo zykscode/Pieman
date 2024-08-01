bc7e11d30fa54d55b305f6dabc025d3094fb26a6
zykcode <zyskson@gmail.com>
2024-08-01 23:30:27 +0100

    feat:user registration

--- app/api/auth/signin.ts
+++ app/api/auth/signin.ts
@@ -1,31 +1,40 @@
-// pages/api/auth/login.js
+import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 import { setCookie } from 'cookies-next';
-import { signIn, SignInResponse } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
 
 const prisma = new PrismaClient();
 
-export default async function handler(req: { method: string; body: { email: any; password: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { error?: string; token?: SignInResponse; }): void; new(): any; }; }; }) {
+interface CustomSignInResponse {
+  ok: boolean;
+  token?: {
+    accessToken: string;
+  };
+  [key: string]: any; // other possible fields
+}
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const { email, password } = req.body;
+  const { email, password } = req.body as { email: string; password: string };
 
   const user = await prisma.user.findUnique({
     where: { email },
   });
 
   if (user && await bcrypt.compare(password, user.password!)) {
-    const token = await signIn('credentials', {
+    const token: CustomSignInResponse | null | undefined = await signIn('credentials', {
       redirect: false,
       email,
       password,
     });
 
-    if (token?.ok) {
-      setCookie('auth-token', token.accessToken, {
+    if (token && token.ok && token.token?.accessToken) {
+      setCookie('auth-token', token.token.accessToken, {
         req,
         res,
         httpOnly: true,
@@ -34,7 +43,7 @@ export default async function handler(req: { method: string; body: { email: any;
         maxAge: 30 * 24 * 60 * 60, // 30 days
         path: '/',
       });
-      res.status(200).json({ token });
+      res.status(200).json({ token: token.token });
     } else {
       res.status(401).json({ error: 'Invalid email or password' });
     }
