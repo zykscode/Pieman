03a72283419c1fc83f7fcdaa1aae4108fb01cb08
zykcode <zyskson@gmail.com>
2024-08-02 00:01:33 +0100

    feat:user registration

---
+++ .eslintrc.js
@@ -0,0 +1,159 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    es6: true,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jsx-a11y',
+    'prettier',
+    'unused-imports',
+    'simple-import-sort',
+    'no-console',
+    'tailwindcss',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'next',
+    'next/core-web-vitals',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/no-unescaped-entities': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'unused-imports/no-unused-imports-ts': 'error',
+    'unused-imports/no-unused-vars-ts': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  overrides: [
+    // Configuration for TypeScript files
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      plugins: [
+        '@typescript-eslint',
+        'unused-imports',
+        'tailwindcss',
+        'simple-import-sort',
+      ],
+      extends: [
+        'plugin:tailwindcss/recommended',
+        'airbnb',
+        'airbnb-typescript',
+        'next/core-web-vitals',
+        'plugin:prettier/recommended',
+      ],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      rules: {
+        'prettier/prettier': [
+          'error',
+          {
+            singleQuote: true,
+            endOfLine: 'auto',
+          },
+        ],
+        'import/extensions': 'off',
+        'react/function-component-definition': 'off',
+        'react/destructuring-assignment': 'off',
+        'react/require-default-props': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        '@typescript-eslint/comma-dangle': 'off',
+        '@typescript-eslint/consistent-type-imports': 'error',
+        'no-restricted-syntax': [
+          'error',
+          'ForInStatement',
+          'LabeledStatement',
+          'WithStatement',
+        ],
+        'import/prefer-default-export': 'off',
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'import/order': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      },
+    },
+    // Configuration for testing
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx'],
+      plugins: ['jest', 'jest-formatting', 'testing-library', 'jest-dom'],
+      extends: [
+        'plugin:jest/recommended',
+        'plugin:jest-formatting/recommended',
+        'plugin:testing-library/react',
+        'plugin:jest-dom/recommended',
+      ],
+    },
+    // Configuration for e2e testing (Playwright)
+    {
+      files: ['**/*.spec.ts'],
+      extends: ['plugin:playwright/recommended'],
+    },
+    // Configuration for Storybook
+    {
+      files: ['*.stories.*'],
+      extends: ['plugin:storybook/recommended'],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: true,
+          },
+        ],
+      },
+    },
+    // Additional TypeScript rules
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': ['warn'],
+      },
+    },
+  ],
+};
