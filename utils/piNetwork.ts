8b43f8315d0cf804e57c0e2acbe7c9f34adf1870
zykcode <zyskson@gmail.com>
2024-08-02 01:57:43 +0100

    feat:user registration

--- utils/piNetwork.ts
+++ utils/piNetwork.ts
@@ -2,9 +2,12 @@
 
 import PiNetwork from 'pi-backend';
 
-const pi = new PiNetwork(
-  process.env.PI_API_KEY!,
-  process.env.walletPrivateSeed!,
-);
+const apiKey =
+  'tprjoipbmycfphacmeocljtu3pbwoghujrkqiifuim32ogjwiehy0w2pypy2efru';
+const walletPrivateSeed =
+  'SCR6OLW5T5HUF6Y5Q52UZ7JH2LNJ4YQKNSHZZFS4WQXYYL22JG72LNET';
+
+console.log(apiKey, walletPrivateSeed);
+const pi = new PiNetwork(apiKey, walletPrivateSeed);
 
 export default pi;
