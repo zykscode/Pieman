3b4696e1fb22b2f09fcc8fb80f9d0f8918193cd4
zykcode <zyskson@gmail.com>
2024-08-05 01:53:54 +0100

    feat:adding clerk authentication

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,6 @@
 'use client';
 
 import { RedirectToSignIn, SignedOut, useUser } from '@clerk/nextjs';
-import { signOut } from 'next-auth/react';
-
-import { Button } from '#/components/ui/button';
 
 export default function Dashboard() {
   const { user, isLoaded, isSignedIn } = useUser();
