9668cf6f73adc654a4ee220617ef368f7d8d8a0b
zykcode <zyskson@gmail.com>
2024-08-06 23:47:39 +0100

    fix:adding clerk auth

--- app/(escrow)/dashboard/page.tsx
+++ app/(escrow)/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { currentUser } from '@clerk/nextjs/server';
 
 export default async function Page() {
   const user = await currentUser();
-
+console.log(user, 'jere is the user')
   if (!user) return <div>Not signed in</div>;
 
   return <div>Hello {user?.emailAddresses[0].emailAddress}</div>;
