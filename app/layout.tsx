b9563899f9820b188f756dc035c7233e2082dbe4
zykcode <zyskson@gmail.com>
2024-09-10 14:37:29 +0100

    feat: rwamda

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,6 @@ import Sidebar from '#/components/Sidebar';
 import { TailwindIndicator } from '#/components/tailwind-indicator';
 import { ThemeProvider } from '#/components/theme-provider';
 import { Toaster } from '#/components/ui/toaster';
-import { AppProvider } from '#/contexts/UserContext';
 import { fontSans } from '#/lib/fonts';
 import { cn } from '#/lib/utils';
 
@@ -37,30 +36,28 @@ export default function RootLayout({
         <ClerkProvider>
           <PiSdkLoader />
 
-          <AppProvider>
-            <AuthProvider>
-              <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-                disableTransitionOnChange
-              >
-                <div className="flex min-h-screen">
-                  <Sidebar />
-                  <div className="flex-1 flex flex-col md:pl-64">
-                    <Header />
-                    <main className="flex-1 overflow-y-auto px-4 py-6 md:px-6">
-                      {children}
-                    </main>
-                    <BottomNav />
-                  </div>
+          <AuthProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <div className="flex min-h-screen">
+                <Sidebar />
+                <div className="flex-1 flex flex-col md:pl-64">
+                  <Header />
+                  <main className="flex-1 overflow-y-auto px-4 py-6 md:px-6">
+                    {children}
+                  </main>
+                  <BottomNav />
                 </div>
-                <Toaster />
-                <Analytics />
-                <TailwindIndicator />
-              </ThemeProvider>
-            </AuthProvider>
-          </AppProvider>
+              </div>
+              <Toaster />
+              <Analytics />
+              <TailwindIndicator />
+            </ThemeProvider>
+          </AuthProvider>
         </ClerkProvider>
       </body>
     </html>
