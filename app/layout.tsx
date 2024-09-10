b2a1be204d7739a6b98976294a6e9fe537e0b83b
zykcode <zyskson@gmail.com>
2024-09-10 14:42:16 +0100

    feat: rwamda

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
       >
         <ClerkProvider>
           <PiSdkLoader />
-
           <AuthProvider>
             <ThemeProvider
               attribute="class"
